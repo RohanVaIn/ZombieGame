e9420da2f5753a670875b2ec597ca86d0797ed7c
RohanVaIn <rohanindukuri08@gmail.com>
2026-02-07 13:13:04 -0500

    Gun
    Added gun and bullet partially, all new sprites

---
+++ zombie-game-quhacks/Bullet.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction* 2000 * delta
